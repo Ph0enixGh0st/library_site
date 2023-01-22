f485989f81c7824d0ff785931e054655aab7e446
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-01-22 23:11:36 +0300

    Split books list by pages.
    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -10,12 +10,29 @@ def on_reload(template):
     with open('books_about.json', 'r', encoding="utf8") as json_file:
         books = json.load(json_file)
 
+    books_per_page = 10
+    chunked_books = list(chunked(books, books_per_page))
+    chunked_book_per_page = 1
+    chunked_pages = list(chunked(chunked_books, chunked_book_per_page))
+    pages_path = Path('pages')
+    pages_path.mkdir(parents=True, exist_ok=True)
+    
+    for page_num, books in enumerate(chunked_pages, 1):
         rendered_page = template.render(
-                books=books,
-            )
-
-        with open('index.html', 'w', encoding="utf-8") as file:
+            books_per_page=books,
+            pages_count=len(chunked_books),
+            current_page=page_num,
+        )
+        with open(pages_path / f'index{page_num}.html', 'w', encoding="utf-8") as file:
             file.write(rendered_page)
+
+
+        #rendered_page = template.render(
+                #books=books,
+            #)
+
+        #with open('index.html', 'w', encoding="utf-8") as file:
+            #file.write(rendered_page)
 
 
 def main():
@@ -29,7 +46,7 @@ def main():
 
     server = Server()
     server.watch('template.html', on_reload(template))
-    server.serve(root='.')
+    server.serve(root='.', default_filename='pages/index1.html')
 
 
 if __name__ == '__main__':
