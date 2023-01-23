1046fb546427ea863cf829307626a1ada8ba3f45
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-01-23 11:40:39 +0300

    pages + render upload
    pages + render upload

---
+++ render_website.py
@@ -0,0 +1,47 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from more_itertools import chunked
+from livereload import Server, shell
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import json, urllib
+from pathlib import Path
+
+
+def on_reload(template):
+    with open('books_about.json', 'r', encoding="utf8") as json_file:
+        books = json.load(json_file)
+
+    books_per_page = 10
+    chunked_books = list(chunked(books, books_per_page))
+
+    chunked_books_per_page = 1
+    chunked_pages = list(chunked(chunked_books, chunked_books_per_page))
+
+    pages_path = Path('pages')
+    pages_path.mkdir(parents=True, exist_ok=True)
+
+    for page_num, books in enumerate(chunked_pages, 1):
+        rendered_page = template.render(
+            books_per_page=books,
+            pages_count=len(chunked_books),
+            current_page=page_num,
+        )
+        with open(f'{pages_path}/index{page_num}.html', 'w', encoding="utf-8") as html_file:
+            html_file.write(rendered_page)
+
+
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+    on_reload(template)
+
+    server = Server()
+    server.watch('template.html', on_reload(template))
+    server.serve(root='.', default_filename='pages/index1.html')
+
+
+if __name__ == '__main__':
+    main()
