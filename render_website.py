0ef7cb8be21e9d7a837d99861f5b7d3e5b8331b5
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-01-25 15:55:20 +0300

    livereload fixed
    livereload fixed

--- render_website.py
+++ render_website.py
@@ -8,10 +8,10 @@ from pathlib import Path
 
 def on_reload(template, books_repository='media/books_about.json'):
     with open(books_repository, 'r', encoding="utf8") as json_file:
-        books = json.load(json_file)
+        books_descriptions = json.load(json_file)
 
     books_per_page = 10
-    chunked_books = list(chunked(books, books_per_page))
+    chunked_books = list(chunked(books_descriptions, books_per_page))
 
     chunked_books_per_page = 1
     chunked_pages = list(chunked(chunked_books, chunked_books_per_page))
@@ -43,7 +43,7 @@ def main():
     on_reload(template, books_repository)
 
     server = Server()
-    server.watch('template.html', on_reload(template))
+    server.watch('template.html', on_reload)
     server.serve(root='.', default_filename='pages/index1.html')
 
 
