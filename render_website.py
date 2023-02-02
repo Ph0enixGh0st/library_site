ef657522c465659b160f7ebf6f07f89bcff6f8b1
Ph0enixGh0st <108229516+Ph0enixGh0st@users.noreply.github.com>
2023-02-02 11:56:50 +0300

    livereload fixed
    livereload fixed

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import urllib
@@ -9,7 +10,23 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def on_reload(template, books_repository):
+def insert_path(on_reload):
+    load_dotenv()
+    books_repository = os.environ.setdefault('BOOKS_REPOSITORY', 'media/books_about.json')
+    def wrapped():
+        return on_reload(books_repository)
+    return wrapped
+
+
+@insert_path
+def on_reload(books_repository):
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+        )
+    template = env.get_template('template.html')
+
     with open(books_repository, 'r', encoding='utf8') as json_file:
         books_descriptions = json.load(json_file)
 
@@ -26,7 +43,7 @@ def on_reload(template, books_repository):
         rendered_page = template.render(
             books_per_page=books,
             pages_count=len(chunked_books),
-            current_page=page_num,
+            current_page=page_num
         )
         with open(f'{pages_path}/index{page_num}.html', 'w', encoding='utf-8') as html_file:
             html_file.write(rendered_page)
@@ -34,20 +51,10 @@ def on_reload(template, books_repository):
 
 def main():
 
-    load_dotenv()
-    books_repository = os.environ.setdefault('BOOKS_REPOSITORY', 'media/books_about.json')
-
-    env = Environment(
-        loader=FileSystemLoader('.'),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
-
-    template = env.get_template('template.html')
-
-    on_reload(template, books_repository)
+    on_reload()
 
     server = Server()
-    server.watch('*.html', on_reload(template, books_repository))
+    server.watch('template.html', on_reload)
     server.serve(root='.', default_filename='pages/index1.html')
 
 
